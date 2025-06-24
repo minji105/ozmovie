107ff2227c04ff04d1de6ed87728a81b1e880f2d
minji_105 <ajhjmj9@naver.com>
2025-06-24 11:23:47 +0900

    🔧 Chore: alias 경로 설정 추가 (#4)

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,8 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@components': path.resolve(__dirname, './src/components'),
     },
   },
 });
