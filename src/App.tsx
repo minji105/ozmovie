7ca7607345882cc168153a1a9c6d2e7274cbd96c
minji_105 <ajhjmj9@naver.com>
2025-06-24 21:15:55 +0900

    ✨ Feat: 공통 레이아웃 구성 (#3)
    - Layout 컴포넌트 생성 및 Outlet으로 하위 페이지 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import './App.css';
 import Home from '@/pages/Home';
 import Login from '@/pages/Login';
 import Register from '@/pages/Resigter';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
