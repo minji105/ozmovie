a4b1108e9c4c999e9d70a3231ffd03bd668e45e8
minji_105 <ajhjmj9@naver.com>
2025-06-24 15:44:03 +0900

    🏗️ Refactor: MediaItem 타입 정의 파일 분리 (#4)

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export interface MediaItem {
+  id: number;
+  media_type: 'tv' | 'movie' | string;
+  title?: string;
+  name?: string;
+  backdrop_path: string;
+  vote_average: number;
+  vote_count: number;
+  original_title?: string;
+  original_name?: string;
+  first_air_date: string;
+  release_date: string;
+  overview: string;
+}
