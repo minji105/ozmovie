694c77f52011e61be8d9f4167d73c217a3dce913
minji_105 <ajhjmj9@naver.com>
2025-06-24 18:06:16 +0900

    ✨ Feat: 로그인 페이지 구현 (#2)
    - react-hook-form + zodResolver로 실시간 유효성 검사 적용
    - supabase.auth.signInWithPassword를 통한 로그인 처리 구현
    - 로그인 성공 시 navigate('/')로 홈 이동, 실패 시 에러 alert

---
+++ src/pages/Login.tsx
@@ -0,0 +1,83 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import supabase from '../supabaseClient';
+import { loginSchema } from '@/lib/validationSchemas';
+import type { LoginSchemaType } from '@/lib/validationSchemas';
+import LOGIN_FIELDS from '@/constants/loginFields';
+import AuthLayout from '@/components/AuthLayout';
+import Input from '@/components/Input';
+
+export default function Login() {
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginSchemaType>({
+    resolver: zodResolver(loginSchema),
+    mode: 'onChange',
+  });
+
+  const handleLogin = async (data: LoginSchemaType) => {
+    try {
+      const { email, password } = data;
+
+      const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+      if (error) {
+        alert('[로그인 실패] ' + error.message);
+        return;
+      }
+
+      navigate('/');
+    } catch (error) {
+      console.error(error);
+    } finally {
+      console.log('로그인 시도 값:', data.email, data.password);
+    }
+  };
+
+  return (
+    <AuthLayout>
+      <form
+        onSubmit={handleSubmit(handleLogin)}
+        className="relative mx-auto mb-20 mt-14 flex w-full min-w-[340px] flex-col gap-4 rounded-2xl bg-black p-[5vw] sm:mt-24 sm:w-[500px] sm:bg-[#000000c1] sm:p-14"
+      >
+        <h2 className="mb-4 text-3xl font-bold sm:text-center">로그인</h2>
+
+        {LOGIN_FIELDS.map(field => (
+          <Input
+            key={field.id}
+            id={field.id}
+            label={field.label}
+            type={field.type}
+            placeholder={field.placeholder}
+            hideLabel={field.hideLabel}
+            error={errors[field.id as keyof LoginSchemaType]?.message}
+            {...register(field.id as keyof LoginSchemaType)}
+          />
+        ))}
+
+        <button
+          type="submit"
+          className="bg-red-primary hover:bg-red-hover w-full rounded-md px-6 py-2 text-lg text-white"
+        >
+          로그인
+        </button>
+
+        <div className="text-base">
+          <span className="text-gray-300">
+            오즈무비 회원이 아닌가요? &nbsp;
+          </span>
+          <Link to="/register">
+            <span className="hover:underline">지금 가입하세요.</span>
+          </Link>
+        </div>
+      </form>
+    </AuthLayout>
+  );
+}
