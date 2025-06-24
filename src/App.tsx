fc726e90db4ece50f84cf35536cb9b2771360f59
minji_105 <ajhjmj9@naver.com>
2025-06-24 18:38:28 +0900

    ✨ Feat: 로그인, 회원가입 페이지 라우트 설정 (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from '@/pages/Home';
+import Login from '@/pages/Login';
+import Register from '@/pages/Resigter';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
