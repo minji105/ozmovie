321c813e9c5e506b13d465841c3bc7638337308f
minji_105 <ajhjmj9@naver.com>
2025-06-24 13:18:45 +0900

    ✨ Feat: TMBD API 요청 로직 커스텀 훅 생성(useFetch) (#4)

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import { API_KEY } from '@constants';
+
+export default function useFetch<T>(query: string) {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setLoading(true);
+
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_KEY}`,
+      },
+    };
+
+    fetch(`https://api.themoviedb.org/3/${query}`, options)
+      .then(res => res.json())
+      .then(resData => {
+        setData(resData);
+        console.log('fetch success ', resData);
+      })
+      .catch(err => console.error(err))
+      .finally(() => setLoading(false));
+  }, [query]);
+
+  return { data, loading };
+}
