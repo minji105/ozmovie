9c9afc9ff80aa004a48497731d2335d8667cc952
minji_105 <ajhjmj9@naver.com>
2025-06-26 04:18:53 +0900

    🔧 Chore: tailwind fontSize 커스터마이징 추가 (#5)

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,12 @@ export default {
       backgroundImage: {
         auth: "url(src/assets/bg.png)",
       },
+      fontSize: {
+        sm: ['14px', '20px'],
+        base: ['16px', '24px'],
+        lg: ['20px', '28px'],
+        xl: ['24px', '32px'],
+      }
     },
   },
   plugins: [],
