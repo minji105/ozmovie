495b303e0745cb28d4a1ba3b326006739d170673
minji_105 <ajhjmj9@naver.com>
2025-06-26 03:35:06 +0900

    ✏️ Fix: Register 컴포넌트명 오타 수정 (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from '@/pages/Home';
 import Login from '@/pages/Login';
-import Register from '@/pages/Resigter';
-import Layout from './components/Layout';
+import Register from '@/pages/Register';
+import Layout from '@/components/Layout';
 
 function App() {
   return (
