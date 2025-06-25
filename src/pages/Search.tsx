50fdd17aeb2f099e9414fefc45007a7e46a69ea4
minji_105 <ajhjmj9@naver.com>
2025-06-26 04:20:52 +0900

    💄 Feat: 검색 결과 UI 개선 및 스켈레톤 로딩 추가 (#5)

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import useFetch from '@/hooks/useFetch';
+import useInfiniteScroll from '@/hooks/useInfiniteScroll';
 import type { MediaItem } from '@/types';
 import MediaCard from '@/components/MediaCard';
-import useInfiniteScroll from '@/hooks/useInfiniteScroll';
+import SkeletonCard from '@/components/skeletons/SkeletonCard';
 
 interface SearchResponse {
   results: MediaItem[];
@@ -46,16 +47,28 @@ export default function Search() {
   const len = resultList.length;
 
   return (
-    <div className="py-10">
-      <p className="fixed bg-black p-4 text-2xl">{len}</p>
-      {!loading &&
-        resultList.map((media: MediaItem) => (
-          <MediaCard
-            key={media.id}
-            title={media.title || media.name || ''}
-            imgSrc={media.poster_path}
-          />
+    <div className="w-full px-[5vw] pb-20 pt-[100px]">
+      <p className="fixed top-0 w-full bg-black pt-[100px] text-base sm:pb-4 sm:text-lg lg:text-xl">
+        `{keyword}` 검색 결과: {len}개{isEnd ? '' : '+'}
+      </p>
+      <div className="grid grid-cols-3 gap-[calc(1vw+8px)] pt-10 sm:grid-cols-4 sm:pt-16 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7">
+        {resultList.map((media: MediaItem) => (
+          <Link to="/" key={`${media.media_type}-${media.id}`}>
+            <MediaCard
+              title={media.title || media.name || ''}
+              imgSrc={media.poster_path}
+            />
+          </Link>
         ))}
+        {loading && (
+          <>
+            <SkeletonCard />
+            <SkeletonCard />
+            <SkeletonCard />
+          </>
+        )}
+      </div>
+
       <div ref={loader} />
     </div>
   );
