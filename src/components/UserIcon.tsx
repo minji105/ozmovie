a224aecf5444ad7fee52ca2f29a2ef34dbbe3a18
minji_105 <ajhjmj9@naver.com>
2025-06-25 16:30:30 +0900

    ✨ Feat: 로그아웃 버튼 추가 (#3)

--- src/components/UserIcon.tsx
+++ src/components/UserIcon.tsx
@@ -2,14 +2,26 @@ import { FaUserCircle } from 'react-icons/fa';
 import { useAuth } from '@/contexts/AuthContext';
 
 export default function UserIcon() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
   const userIcon = user?.user_metadata.avatar_url;
 
-  return userIcon ? (
-    <div className="w-[30px] cursor-pointer overflow-hidden rounded-full">
-      <img src={userIcon} alt="user icon" />
+  return (
+    <div className="group relative">
+      {userIcon ? (
+        <div className="w-[30px] cursor-pointer overflow-hidden rounded-full">
+          <img src={userIcon} alt="user icon" />
+        </div>
+      ) : (
+        <FaUserCircle className="cursor-pointer text-3xl" />
+      )}
+
+      <div className="absolute right-0 top-0 hidden w-max group-hover:block">
+        <div className="mt-10 bg-[#000000c1] p-4 text-sm">
+          <button type="button" className="hover:underline" onClick={signOut}>
+            로그아웃
+          </button>
+        </div>
+      </div>
     </div>
-  ) : (
-    <FaUserCircle className="cursor-pointer text-3xl" />
   );
 }
