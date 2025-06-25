7e8342ea1309c1ef90db1276ef43bd948abc6222
minji_105 <ajhjmj9@naver.com>
2025-06-26 03:43:32 +0900

    ✨ Feat: 검색 페이지에 무한 스크롤 기능 추가 (#5)

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,32 +1,62 @@
+import { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import useFetch from '@/hooks/useFetch';
 import type { MediaItem } from '@/types';
 import MediaCard from '@/components/MediaCard';
+import useInfiniteScroll from '@/hooks/useInfiniteScroll';
 
 interface SearchResponse {
   results: MediaItem[];
+  page: number;
+  total_pages: number;
 }
 
 export default function Search() {
   const [searchParams] = useSearchParams();
   const keyword = searchParams.get('keyword');
 
+  const [resultList, setResultList] = useState<MediaItem[]>([]);
+  const [page, setPage] = useState<number>(1);
+  const [isEnd, setIsEnd] = useState<boolean>(false);
+
   const { data, loading } = useFetch<SearchResponse>(
-    `search/multi?query=${keyword}&include_adult=false&language=ko`,
+    `search/multi?query=${keyword}&include_adult=false&language=ko&page=${page}`,
   );
 
-  const filtered = data?.results?.filter(el => el.backdrop_path) || [];
+  const loader = useInfiniteScroll(
+    () => setPage(prev => prev + 1),
+    !loading && !isEnd,
+  );
+
+  useEffect(() => {
+    setPage(1);
+    setResultList([]);
+    setIsEnd(false);
+  }, [keyword]);
+
+  useEffect(() => {
+    if (data?.results) {
+      const filtered = data.results.filter(el => el.backdrop_path);
+      setResultList(prev => [...prev, ...filtered]);
+
+      if (data.page >= data.total_pages) setIsEnd(true);
+    }
+  }, [data]);
+
+  const len = resultList.length;
 
   return (
-    <>
+    <div className="py-10">
+      <p className="fixed bg-black p-4 text-2xl">{len}</p>
       {!loading &&
-        filtered.map((media: MediaItem, index: number) => (
+        resultList.map((media: MediaItem) => (
           <MediaCard
-            key={index}
+            key={media.id}
             title={media.title || media.name || ''}
             imgSrc={media.poster_path}
           />
         ))}
-    </>
+      <div ref={loader} />
+    </div>
   );
 }
