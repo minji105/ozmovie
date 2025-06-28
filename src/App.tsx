4065cb1aab6e80d99674a3122c7b04dbcd66f3a1
minji_105 <ajhjmj9@naver.com>
2025-06-28 18:39:40 +0900

    🔥 Feat: Detail 페이지 라우트 삭제 (#6)

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Login from '@/pages/Login';
 import Register from '@/pages/Register';
 import Layout from '@/components/Layout';
 import Search from '@/pages/Search';
-import Detail from './pages/Detail';
+// import Detail from './pages/Detail';
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="search" element={<Search />} />
-          <Route path="detail/:type/:id" element={<Detail />} />
+          {/* <Route path="detail/:type/:id" element={<Detail />} /> */}
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
