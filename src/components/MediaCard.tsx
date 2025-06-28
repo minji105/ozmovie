3c704a9143164b42a5dcc701c2c986b63fc34d21
minji_105 <ajhjmj9@naver.com>
2025-06-29 03:52:22 +0900

    ♻️ Refactor: MediaCard 컴포넌트 내부에서 Link 처리하도록 구조 변경 (#17)
    호출부에서 Link로 감싸는 대신, path prop을 전달하여 컴포넌트 내부에서 Link 처리

--- src/components/MediaCard.tsx
+++ src/components/MediaCard.tsx
@@ -1,16 +1,20 @@
 import { BASE_URL } from '@/constants';
+import { Link } from 'react-router-dom';
 
 type MediaCardProps = {
   title: string;
   imgSrc: string;
+  path: string;
 };
 
-export default function MediaCard({ title, imgSrc }: MediaCardProps) {
+export default function MediaCard({ title, imgSrc, path }: MediaCardProps) {
   return (
-    <img
-      className="aspect-[0.7] rounded-sm object-cover"
-      src={`${BASE_URL}${imgSrc}`}
-      alt={title}
-    />
+    <Link to={path}>
+      <img
+        className="aspect-[0.7] rounded-sm object-cover"
+        src={`${BASE_URL}${imgSrc}`}
+        alt={title}
+      />
+    </Link>
   );
 }
--- src/components/detailModal/Recommandation.tsx
+++ src/components/detailModal/Recommandation.tsx
@@ -5,8 +5,13 @@ export default function Recommandation({ data }: { data: any }) {
     <>
       <h2 className="mt-10 text-xl">함께 시청된 콘텐츠</h2>
       <div className="grid grid-cols-[repeat(4,1fr)] gap-6">
-        {data.results.map((el: any) => (
-          <MediaCard key={el.id} title={el.title} imgSrc={el.poster_path} />
+        {data?.results.map((el: any) => (
+          <MediaCard
+            key={el.id}
+            title={el.title}
+            imgSrc={el.poster_path}
+            path={`?type=${el.media_type}&id=${el.id}`}
+          />
         ))}
       </div>
     </>
