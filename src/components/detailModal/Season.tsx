2d60224e9594ce78c92c0b0c65f9cb1e24e94f6f
minji_105 <ajhjmj9@naver.com>
2025-06-29 20:45:25 +0900

    💄 Style: Season 컴포넌트 스타일 적용 (#18)

--- src/components/detailModal/Season.tsx
+++ src/components/detailModal/Season.tsx
@@ -26,37 +26,47 @@ export default function Season({ id, seriesLength }: Props) {
 
   return (
     <section>
-      <h2 className="mb-6 text-xl">회차</h2>
+      <div className="flex justify-between">
+        <h2 className="text-xl">회차</h2>
 
-      <select
-        value={selectedSeason}
-        onChange={e => setSelectedSeason(Number(e.target.value))}
-        className="ex mb-6 rounded border px-2 py-1 text-black"
-      >
-        {Array.from({ length: seriesLength }, (_, i) => (
-          <option key={i + 1} value={i + 1}>
-            시즌 {i + 1}
-          </option>
-        ))}
-      </select>
+        <select
+          id="season"
+          name="season"
+          value={selectedSeason}
+          onChange={e => setSelectedSeason(Number(e.target.value))}
+          className="rounded border border-stone-500 bg-stone-800 px-2 py-2 outline-none"
+        >
+          {Array.from({ length: seriesLength }, (_, i) => (
+            <option key={i + 1} value={i + 1}>
+              시즌 {i + 1}
+            </option>
+          ))}
+        </select>
+      </div>
 
-      {seasonData && (
-        <div>
-          <p>{seasonData.name}</p>
-          {seasonData.episodes?.map(episode => {
+      {seasonData && !loading && (
+        <div className="divide-y divide-gray-600">
+          {seasonData.episodes?.map((episode, index) => {
             const { runtime } = parseMediaInfo(episode, 'tv');
 
             return (
-              <div key={episode.id} className="flex">
+              <div
+                key={episode.id}
+                className="mb-2 flex items-center gap-4 p-8"
+              >
+                <p className="text-xl text-gray-300">{index + 1}</p>
                 <img
                   src={`${BASE_URL}${episode.still_path}`}
                   alt={episode.name}
+                  className="w-[25%]"
                 />
-                <div>
-                  <p>{episode.name}</p>
-                  <p>{episode.air_date}</p>
-                  <p>{runtime}</p>
-                  <p>{episode.overview}</p>
+                <div className="flex w-full flex-col gap-2">
+                  <p className="font-semibold">{episode.name}</p>
+                  <div className="flex justify-between text-sm text-gray-300">
+                    <p>{episode.air_date}</p>
+                    <p>{runtime}</p>
+                  </div>
+                  <p className="text-sm text-gray-100">{episode.overview}</p>
                 </div>
               </div>
             );
