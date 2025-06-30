e3ea040aa781c46b0ed7fd07222ff151e34e41ad
minji_105 <ajhjmj9@naver.com>
2025-06-30 23:21:34 +0900

    📱 Style: Recommendation 컴포넌트 레이아웃 스타일 개선 (#14)

--- src/components/detailModal/Recommendation.tsx
+++ src/components/detailModal/Recommendation.tsx
@@ -3,8 +3,10 @@ import MediaCard from '../MediaCard';
 export default function Recommendation({ data }: { data: any }) {
   return (
     <section>
-      <h2 className="mb-6 text-xl">함께 시청된 콘텐츠</h2>
-      <div className="grid grid-cols-[repeat(4,1fr)] gap-6">
+      <h2 className="mb-5 inline-block border-b-4 border-red-700 pb-2 text-xl sm:mb-6">
+        함께 시청된 콘텐츠
+      </h2>
+      <div className="grid grid-cols-[repeat(3,1fr)] gap-3 sm:grid-cols-[repeat(4,1fr)] sm:gap-4">
         {data?.results.map((el: any) => (
           <MediaCard
             key={el.id}
