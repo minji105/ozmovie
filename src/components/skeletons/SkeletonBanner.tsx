8c39f72fd321e72a3386a16927e5019be922c3b6
minji_105 <ajhjmj9@naver.com>
2025-07-01 00:10:49 +0900

    📱 Style: 배너 스켈레톤 컴포넌트 반응형 디자인 개선 (#14)

--- src/components/skeletons/SkeletonBanner.tsx
+++ src/components/skeletons/SkeletonBanner.tsx
@@ -2,14 +2,14 @@ export default function SkeletonBanner() {
   return (
     <div className="banner-responsive relative animate-pulse">
       <div className="banner-responsive banner-gradient fixed z-0 w-full bg-gray-900" />
-      <div className="banner-info">
-        <div className="h-[5vw] w-1/3 rounded bg-gray-600" />
-        <div className="h-[calc(1vw+4px)] w-1/2 rounded bg-gray-700" />
-        <div className="h-[calc(1vw+4px)] w-[calc(100%-5vw)] rounded bg-gray-700 md:w-[calc(50%-5vw)]" />
-        <div className="h-[calc(1vw+4px)] w-[calc(100%-5vw)] rounded bg-gray-700 md:w-[calc(50%-5vw)]" />
+      <div className="banner-info responsive-spacing w-full">
+        <div className="h-12 w-1/3 rounded bg-gray-600" />
+        <div className="h-4 w-1/2 rounded bg-gray-700" />
+        <div className="h-4 w-full rounded bg-gray-700 md:w-1/2" />
+        <div className="h-4 w-full rounded bg-gray-700 md:w-1/2" />
         <div className="flex gap-4">
-          <div className="h-[calc(3.6vw+6px)] w-[calc(6.2vw+40px)] rounded-lg bg-gray-600" />
-          <div className="h-[calc(3.6vw+6px)] w-[calc(6.2vw+72px)] rounded-lg bg-gray-600" />
+          <div className="h-10 w-20 rounded-md bg-gray-600 sm:w-24" />
+          <div className="h-10 w-24 rounded-md bg-gray-600 sm:w-32" />
         </div>
       </div>
     </div>
