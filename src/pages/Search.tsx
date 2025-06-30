aca7edbf7523c787064dc523d48fcb1efcdf847e
minji_105 <ajhjmj9@naver.com>
2025-06-30 23:45:29 +0900

    💄 Style: 검색 결과 패딩 속성 수정 (#14)

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -57,7 +57,7 @@ export default function Search() {
 
   return (
     <div className="w-full px-[5vw] pb-20 pt-[100px]">
-      <p className="fixed top-0 w-full bg-black pt-[100px] text-base sm:pb-4 sm:text-lg lg:text-xl">
+      <p className="fixed top-0 w-full bg-black pb-4 pt-[100px] text-base sm:text-lg lg:text-xl">
         `{keyword}` 검색 결과: {len}개{isEnd ? '' : '+'}
       </p>
       <div className="grid grid-cols-3 gap-[calc(1vw+8px)] pt-10 sm:grid-cols-4 sm:pt-16 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7">
