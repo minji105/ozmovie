bc9da50860fcb7389f892117d474aa458510e9c8
minji_105 <ajhjmj9@naver.com>
2025-07-05 18:32:34 +0900

    ✨ Feat: 마이페이지에 좋아요한  목록 렌더링 (#28)

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -2,21 +2,30 @@ import { useFavorites } from '@/contexts/FavoriteContext';
 import MediaCard from '@/components/MediaCard';
 import DetailModal from '@/components/detailModal/DetailModal';
 import { useDetailModal } from '@/hooks/useDetailModal';
+import { useLikes } from '@/contexts/LikeContext';
 
 export default function Mypage() {
   const { type, id, closeModal } = useDetailModal();
-  const { favorites, loading } = useFavorites();
-
-  if (loading) return <p className="text-center text-white">불러오는 중...</p>;
-
-  if (favorites.length === 0)
-    return <p className="text-center text-gray-400">찜한 콘텐츠가 없습니다.</p>;
+  const { favorites } = useFavorites();
+  const { likes } = useLikes();
 
   return (
     <>
       <h2>내가 찜한 리스트</h2>
       <div className="grid grid-cols-2 gap-4 p-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
         {favorites.map(item => (
+          <MediaCard
+            key={item.id}
+            title={item.title}
+            imgSrc={item.poster_path || ''}
+            path={`?type=${item.media_type}&id=${item.id}`}
+          />
+        ))}
+      </div>
+
+      <h2>마음에 들어 하신 시리즈와 영화</h2>
+      <div className="grid grid-cols-2 gap-4 p-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+        {likes.map(item => (
           <MediaCard
             key={item.id}
             title={item.title}
