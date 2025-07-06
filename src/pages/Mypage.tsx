6a3549b94a4e4e92ea9d1c1eea46d9f8b4d0db2b
minji_105 <ajhjmj9@naver.com>
2025-07-06 19:51:05 +0900

    ✨ Feat: Mypage에 SliderSection 컴포넌트 적용 (#30)

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,38 +1,41 @@
 import { useFavorites } from '@/contexts/FavoriteContext';
-import MediaCard from '@/components/MediaCard';
 import DetailModal from '@/components/detailModal/DetailModal';
 import { useDetailModal } from '@/hooks/useDetailModal';
 import { useLikes } from '@/contexts/LikeContext';
+import SliderSection from '@/components/SliderSection';
 
 export default function Mypage() {
   const { type, id, closeModal } = useDetailModal();
-  const { favorites } = useFavorites();
-  const { likes } = useLikes();
+  const { favorites, loading: favoritesLoading } = useFavorites();
+  const { likes, loading: likesLoading } = useLikes();
+
+  const sliders = [
+    {
+      title: '내가 찜한 리스트',
+      data: favorites || [],
+      loading: favoritesLoading,
+    },
+    {
+      title: '마음에 들어 하신 시리즈와 영화',
+      data: likes || [],
+      loading: likesLoading,
+    },
+  ];
 
   return (
     <>
-      <h2>내가 찜한 리스트</h2>
-      <div className="grid grid-cols-2 gap-4 p-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-        {favorites.map(item => (
-          <MediaCard
-            key={item.id}
-            title={item.title}
-            imgSrc={item.poster_path || ''}
-            path={`?type=${item.media_type}&id=${item.id}`}
-          />
-        ))}
-      </div>
-
-      <h2>마음에 들어 하신 시리즈와 영화</h2>
-      <div className="grid grid-cols-2 gap-4 p-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-        {likes.map(item => (
-          <MediaCard
-            key={item.id}
-            title={item.title}
-            imgSrc={item.poster_path || ''}
-            path={`?type=${item.media_type}&id=${item.id}`}
-          />
-        ))}
+      <div className="w-full">
+        {sliders.map(
+          (slider, idx) =>
+            !slider.loading && (
+              <SliderSection
+                key={idx}
+                title={slider.title}
+                data={slider.data}
+                type={null}
+              />
+            ),
+        )}
       </div>
 
       {type && id && <DetailModal type={type} id={id} onClose={closeModal} />}
