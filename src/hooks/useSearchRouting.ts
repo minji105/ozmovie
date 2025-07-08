f6de2895a3838ba1884c82cd1c9b5c56ea98eef8
minji_105 <ajhjmj9@naver.com>
2025-07-08 23:56:43 +0900

    🐛 Fix: 검색 페이지에서 이전 경로로 이동하도록 수정 (#35)
    검색어를 지우면 무조건 홈(/)으로 이동하는 문제
    -> useRef를 사용하여 이전 경로를 기억하고 검색어를 지우면 이전 경로로 이동하도록 수정

--- src/hooks/useSearchRouting.ts
+++ src/hooks/useSearchRouting.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import useDebounce from '@/hooks/useDebounce';
 
@@ -7,13 +7,20 @@ export default function useSearchRouting() {
   const debouncedValue = useDebounce(inputDebounce);
   const navigate = useNavigate();
   const location = useLocation();
+  const prevPathRef = useRef<string>(location.pathname);
+
+  useEffect(() => {
+    if (location.pathname !== '/search') {
+      prevPathRef.current = location.pathname;
+    }
+  }, [location.pathname]);
 
   useEffect(() => {
     if (debouncedValue.trim()) {
       navigate(`/search?keyword=${debouncedValue}`);
       console.log('keyword: ', debouncedValue);
     } else if (location.pathname.startsWith('/search')) {
-      navigate('/');
+      navigate(prevPathRef.current);
     }
   }, [debouncedValue, location.pathname, navigate]);
 
